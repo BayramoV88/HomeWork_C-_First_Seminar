9dba41c2aabe86d1b72c648479c83fe616743977
Vadim <bayramovva@gmail.com>
2022-07-11 17:45:41 +0300

    Add Zadacha 2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int number_a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number_b = Convert.ToInt32(Console.ReadLine());
+
+if (number_a > number_b)
+{
+    Console.WriteLine("Максимальное число " + number_a);
+}
+else
+{
+    Console.WriteLine("Максимальное число " + number_b);
+}
