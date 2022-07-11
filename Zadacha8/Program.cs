9fd14e045d8b3f64dd5c99c59501158adea75aee
Vadim Bayramov <bayramovva@gmail.com>
2022-07-12 02:04:32 +0300

    Add Zadacha8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int start = 1;
+while (start <= number)
+{
+    if (start % 2 == 0)
+    {
+        Console.Write(start + " ");
+        start++;
+    }
+    else
+    {
+        start++;
+    }
+}
+Console.WriteLine();
