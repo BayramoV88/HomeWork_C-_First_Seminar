346e7a70abe5a02f7328209e2767d49260c5f24d
Vadim <bayramovva@gmail.com>
2022-07-11 18:18:55 +0300

    Add Zadacha4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите первое число: ");
+int number_a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number_b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number_c = Convert.ToInt32(Console.ReadLine());
+
+if (number_a > number_b)
+{
+    if (number_a > number_c)
+    {
+        Console.WriteLine("Максимальное число " + number_a);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число " + number_c);
+    }
+}
+else if (number_b > number_c)
+{
+    Console.WriteLine("Максимальное число " + number_b);
+}
+else
+{
+    Console.WriteLine("Максимальное число " + number_c);
+}
